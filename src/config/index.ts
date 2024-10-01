1c60d6f2b0f3c8b5cf0b28350f9f787ffa85c3f4
SerhiiD <>
2024-10-01 13:25:54 +0300

    Add db connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,17 @@ import 'dotenv/config';
 
 export type Config = {
   PORT: string;
+  DATABASE_URL: string;
 }
 
 const {
-  PORT
+  PORT,
+  DATABASE_URL
 } = process.env;
 
 const config = {
   PORT: PORT || '3000',
+  DATABASE_URL: DATABASE_URL || '',
 }
 
 export default config as Config;
