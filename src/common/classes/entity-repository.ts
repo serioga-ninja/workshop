89ae2f1c82148a49b328533c9b834c93dc7c2839
SerhiiD <>
2024-10-07 17:15:28 +0300

    Add a few default methods for the entity-re

--- src/common/classes/entity-repository.ts
+++ src/common/classes/entity-repository.ts
@@ -1,8 +1,27 @@
+import { randomUUID } from 'node:crypto';
 import { EntityStatus } from '../constants';
 import EntityBase from './entity-base';
 import RepositoryBase from './repository';
 
 export default abstract class RepositoryEntity<Entity extends EntityBase> extends RepositoryBase<Entity> {
+
+  override async createOne(data: Partial<Entity>): Promise<Entity> {
+    return super.createOne({
+      id: randomUUID(),
+      entityStatus: EntityStatus.Active,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ...data,
+    });
+  }
+
+  override async updateOneBy(where: Partial<Entity>, data: Partial<Entity>): Promise<Entity> {
+    return super.updateOneBy(where, {
+      updatedAt: new Date(),
+      ...data,
+    });
+  }
+
   async softDeleteOneBy(where: Partial<Entity>) {
     const data = {
       entityStatus: EntityStatus.Deleted
